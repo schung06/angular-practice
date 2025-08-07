d9293ce3b731266c7736bffa227b4724c0bf9a5e
Stephanie <stephaniec328122716@gmail.com>
2025-08-07 13:10:22 -0700

    add reusable component for dashboard

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard-item',
+    standalone: true,
+    templateUrl: './dashboard-item.component.html',
+    styleUrls: ['./dashboard-item.component.css'],
+})
+
+export class DashboardItemComponent {
+    @Input({ required: true }) image!: { src: string, alt: string};
+    @Input({ required: true }) title!: string;
+}
