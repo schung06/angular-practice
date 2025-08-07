1ba920dfc85cd74cd2b5d635aac2a58f3b2056f2
Stephanie <stephaniec328122716@gmail.com>
2025-08-07 13:44:09 -0700

    button attribute selector

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../shared/button/button.component";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
+    imports: [ButtonComponent],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
